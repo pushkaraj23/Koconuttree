de249e4eb25784f00ca801070ee4e51d868b2710
pushkaraj23 <pushkaraj232002@gmail.com>
2025-12-16 18:53:18 +0530

    done

--- app/menu/page.js
+++ app/menu/page.js
@@ -1,10 +1,7 @@
-
-const page = () => {
+export default function MenuPage() {
   return (
-    <div className="w-full h-full bg-black text-white">
-      This is the Menu Page. 
+    <div className="w-full min-h-screen bg-black text-white">
+      This is the Menu Page.
     </div>
-  )
+  );
 }
-
-export default page
